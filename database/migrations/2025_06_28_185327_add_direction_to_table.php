837526226a19a09b51a5b158c84c00f88fcbe424
jeremie <jeremie-codes>
2025-07-07 07:47:17 +0100

    kekek

--- database/migrations/2025_06_28_185327_add_direction_to_table.php
+++ database/migrations/2025_06_28_185327_add_direction_to_table.php
@@ -10,17 +10,17 @@ return new class extends Migration
     {
         Schema::table('visitors', function (Blueprint $table) {
             // Ajouter les clés étrangères pour direction et service
-            $table->foreignId('direction_id')->nullable()->constrained('directions')->onDelete('set null');
+            // $table->foreignId('direction_id')->nullable()->constrained('directions')->onDelete('set null');
 
-            // Index pour améliorer les performances
-            $table->index('direction_id');
+            // // Index pour améliorer les performances
+            // $table->index('direction_id');
         });
     }
 
     public function down(): void
     {
         Schema::table('agents', function (Blueprint $table) {
-            $table->dropForeign(['direction_id']);
+            $table->dropForeign('direction_id');
         });
     }
 };
